681b283160dced84c7c0805622652cb69af5e2a2
MrsMetaMorfa <maitressse.metamorfa@gmail.com>
2020-03-18 23:38:40 +0500

    Add all scripts

--- README.md
+++ README.md
@@ -2,20 +2,28 @@
 
 https://mrsmetamorfa.github.io/russian-freedom.github.io/
 
-'images/content' - иконки. По-хорошему нужно собрать в спрайт, но оставила на случай, если нужно будет заменить.
+```images/content``` - иконки. По-хорошему нужно собрать в спрайт, но оставила на случай, если нужно будет заменить.
 
-'images/general' - картинки и логотипы. И флаг.
+```images/general``` - картинки и логотипы. И флаг.
 
 В будущем изменится структура стилей:
-	'main.css' - общие для всех сайтов.
-	'children.css' - для дочерних, на основном подключать не нужно.
-	'arctic.css, baikal.css, moscow.css, petersburg.css' - файлы тем, там будут только цвета.
+	```main.css``` - общие для всех сайтов.
+	```children.css``` - для дочерних, на основном подключать не нужно.
+	```arctic.css, baikal.css, moscow.css, petersburg.css``` - файлы тем, там будут только цвета.
 	
 Тема меняется изменением дополнительного класса для 'main.main':
-	'.arctic, .baikal, .moscow, .petersburg' - добавляют нужную тему, 
+	```.arctic, .baikal, .moscow, .petersburg``` - добавляют нужную тему, 
 	отсутствие этих классов оставляет тему дефолтной.
 	
-'js/main.js' тоже разделится на два, в 'children.js' перемесятся скрипты дочерних сайтов.
+```js/main.js``` тоже разделится на два, в ```children.js``` перемесятся скрипты дочерних сайтов.
 
 На данный момент html-структура неизменна для всех страниц.
 Все остальные изменения коснуться только стилей и скриптов.
+
+**Удалить отовсюду onclick**
+** Ссылки изменились на кнопки внутри .tours-carousel_slides**
+```<a class="btn btn-arrow" href=# aria-label=""></a>``` 
+=>
+```<button class="btn btn-arrow btn-prev" type="button" aria-label="Prev"></button>```
+и 
+```<button class="btn btn-arrow btn-next" type="button" aria-label="Next"></button>```
--- static/js/main.js
+++ static/js/main.js
@@ -47,7 +47,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   //The first argument are the elements to which the plugin shall be initialized
   //The second argument has to be at least a empty object or a object with your desired options
-  OverlayScrollbars(document.querySelectorAll("body"), {});
+  OverlayScrollbars(document.querySelectorAll("body"), {}); // let instance = OverlayScrollbars(document.getElementById(id), { /* your options */ });
+
+  var headerMenu = OverlayScrollbars(document.querySelectorAll('.header_menu-nav'), {}),
+      tabContent = OverlayScrollbars(document.querySelectorAll('.tabs .tabs_content'), {}),
+      modalBlock = OverlayScrollbars(document.querySelectorAll('.modals .modal'), {});
 }); //Header
 
 var headerMenu = document.querySelector('header .header_menu'),
@@ -122,7 +126,12 @@ Siema.prototype.addPagination = function () {
     btn.setAttribute('type', 'button');
     btn.textContent = i;
     btn.addEventListener('click', function () {
-      return _this.goTo(i);
+      return _this.goTo(i, function () {
+        btnContainer.querySelector('.btn.active').classList.remove('active');
+        var btnList = btnContainer.querySelectorAll('.btn'),
+            target = btnList[i];
+        target.classList.add('active');
+      });
     });
     btnContainer.appendChild(btn);
   };
@@ -138,12 +147,35 @@ if (document.querySelector('.feedback')) {
   var feedback = document.querySelector('.feedback');
   var feedbackCarousel = new Siema({
     selector: '.feedback',
-    perPage: 2
+    perPage: {
+      320: 1,
+      768: 2
+    },
+    loop: true,
+    onChange: function onChange() {
+      var dotsList = document.querySelectorAll('.feedback .pagination .btn'),
+          dotActive = document.querySelector('.feedback .pagination .btn.active'),
+          target = this.currentSlide;
+      dotActive.classList.remove('active');
+
+      if (target < 0) {
+        target = dotsList.length + target;
+        dotsList[target].classList.add('active');
+      } else {
+        dotsList[target].classList.add('active');
+      }
+    }
   });
   feedbackCarousel.addPagination();
+  console.log(document.querySelectorAll('.feedback .pagination .btn'));
+  setInterval(function () {
+    return feedbackCarousel.next();
+  }, 5000);
 }
 
 if (document.querySelector('.main--tours .tours_list--inner')) {
+  var toursListScroll = function toursListScroll(e) {};
+
   var toursListInner = document.querySelector('.main--tours .tours_list--inner'),
       toursList = document.querySelector('.main--tours .tours_list'),
       toursListTop = toursList.offsetTop,
@@ -171,8 +203,6 @@ if (document.querySelector('.main--tours .tours_list--inner')) {
       }
 
       for (var r = 0; r < toursItemList.length; r++) {
-        console.log(toursItemList[r].offsetTop - translate, toursListTop);
-
         if (translate - toursItemList[r].offsetTop > toursListTop) {
           toursItemList[r].classList.add('opaque');
         } else {
@@ -183,4 +213,176 @@ if (document.querySelector('.main--tours .tours_list--inner')) {
       toursListInner.style.transform = "translateY(" + translate * -1 + "px)";
     }
   };
+}
+
+if (document.querySelector('.tours-carousel_slides')) {
+  (function () {
+    var ChangeSlide = function ChangeSlide(index) {
+      var target = index + 1;
+
+      if (contentMain.classList.contains('hidden')) {
+        for (var r = 0; r < contentList.length; r++) {
+          contentList[r].classList.remove('current');
+          imagesList[r].classList.remove('current');
+        }
+
+        contentList[target].classList.add('current');
+        imagesList[target].classList.add('current');
+      } else {
+        contentWrapper.classList.add('active');
+        contentList[target].classList.add('current');
+        contentMain.classList.add('hidden');
+        imagesListWrapper.classList.add('active');
+        imagesList[target].classList.add('current');
+        imagesListMain.classList.add('hidden');
+      }
+    };
+
+    var ChangeSlideToMain = function ChangeSlideToMain() {
+      for (var r = 0; r < contentList.length; r++) {
+        contentList[r].classList.remove('current');
+        imagesList[r].classList.remove('current');
+      }
+
+      contentWrapper.classList.remove('active');
+      contentMain.classList.remove('hidden');
+      imagesListWrapper.classList.remove('active');
+      imagesListMain.classList.remove('hidden');
+    };
+
+    console.log('Slides Founded!');
+    var slides = document.querySelector('.tours-carousel_slides'),
+        slidesList = slides.querySelectorAll('.tours-list_item .btn'),
+        imagesList = document.querySelectorAll('.image-background img'),
+        imagesListWrapper = document.querySelector('.image-background'),
+        imagesListMain = document.querySelector('.image-background .main'),
+        contentList = document.querySelectorAll('.main .content--main'),
+        contentWrapper = document.querySelector('.tours-carousel_content'),
+        contentMain = contentList[0];
+
+    var _loop2 = function _loop2(slideIndex) {
+      slidesList[slideIndex].addEventListener('mouseenter', function () {
+        ChangeSlide(slideIndex);
+      });
+      slides.querySelector('.tours-list').addEventListener('mouseleave', function () {
+        ChangeSlideToMain();
+      });
+    };
+
+    for (var slideIndex = 0; slideIndex < slidesList.length; slideIndex++) {
+      _loop2(slideIndex);
+    }
+
+    var slidesCarouselPrev = slides.querySelector('.btn-prev'),
+        slidesCarouselNext = slides.querySelector('.btn-next'),
+        slidesCarouselCurrentPrev;
+    var slidesCarousel = new Siema({
+      selector: '.tours-list',
+      perPage: {
+        320: 1,
+        640: 2,
+        900: 3,
+        1200: 4
+      },
+      onInit: function onInit() {
+        console.log(this.currentSlide);
+      },
+      onChange: function onChange() {}
+    });
+    slidesCarouselNext.addEventListener('click', function () {
+      slidesCarouselCurrentPrev = slidesCarousel.currentSlide;
+      slidesCarousel.next();
+
+      if (slidesCarouselCurrentPrev == slidesCarousel.currentSlide) {
+        ChangeSlide(slidesCarousel.currentSlide);
+        var slidesListActive = slides.querySelector('.tours-list_item .btn.active');
+
+        if (slidesListActive) {
+          slidesListActive.classList.remove('active');
+        }
+
+        slidesList[slidesCarousel.currentSlide].classList.add('active');
+
+        if (slidesCarousel.currentSlide < slidesCarousel.innerElements.length - 1) {
+          slidesCarousel.currentSlide = slidesCarouselCurrentPrev + 1;
+        }
+      } else if (slidesCarouselCurrentPrev < slidesCarousel.currentSlide) {
+        ChangeSlide(slidesCarousel.currentSlide);
+
+        var _slidesListActive = slidesList.querySelector('.btn.active');
+
+        if (_slidesListActive) {
+          _slidesListActive.classList.remove('active');
+        }
+
+        slidesList[slidesCarousel.currentSlide].classList.add('active');
+      }
+    });
+    slidesCarouselPrev.addEventListener('click', function () {
+      slidesCarouselCurrentPrev = slidesCarousel.currentSlide;
+      slidesCarousel.prev();
+
+      if (slidesCarousel.currentSlide > 0) {
+        if (slidesCarouselCurrentPrev == slidesCarousel.currentSlide) {
+          ChangeSlide(slidesCarousel.currentSlide - 1);
+          var slidesListActive = slides.querySelector('.tours-list_item .btn.active');
+
+          if (slidesListActive) {
+            slidesListActive.classList.remove('active');
+          }
+
+          slidesList[slidesCarousel.currentSlide - 1].classList.add('active');
+
+          if (slidesCarouselCurrentPrev > 1) {
+            slidesCarousel.currentSlide = slidesCarouselCurrentPrev - 1;
+          }
+        } else if (slidesCarouselCurrentPrev > slidesCarousel.currentSlide) {
+          console.log(slidesCarouselCurrentPrev, slidesCarousel.currentSlide);
+          ChangeSlide(slidesCarousel.currentSlide);
+
+          var _slidesListActive2 = slidesList.querySelector('.btn.active');
+
+          if (_slidesListActive2) {
+            _slidesListActive2.classList.remove('active');
+          }
+
+          slidesList[slidesCarousel.currentSlide].classList.add('active');
+        }
+      }
+    });
+  })();
+}
+
+if (document.querySelector('.main--tours .tour-info .tabs')) {
+  (function () {
+    var ToggleTab = function ToggleTab(target) {
+      for (var tab = 0; tab < tabsList.length; tab++) {
+        tabsList[tab].classList.remove('active');
+        contentList[tab].classList.remove('active');
+      }
+
+      tabsList[target].classList.add('active');
+      contentList[target].classList.add('active');
+    };
+
+    console.log('Tabs founded!');
+    var tabsList = document.querySelectorAll('.tabs .tab'),
+        contentList = document.querySelectorAll('.tabs_content .tab_content'),
+        buttonMore = document.querySelector('.main--tours .content--tour .btn-more'),
+        tourInfoblock = document.querySelector('.main--tours .tour-info'),
+        tourTitleblock = document.querySelector('.main--tours .content--tour');
+    buttonMore.addEventListener('click', function () {
+      tourInfoblock.classList.add('show');
+    });
+
+    var _loop3 = function _loop3(_t) {
+      tabsList[_t].addEventListener('click', function () {
+        ToggleTab(_t);
+      });
+    };
+
+    for (var _t = 0; _t < tabsList.length; _t++) {
+      _loop3(_t);
+    }
+  })();
 }
